a1af89f63f3a22da8e6ef065171ad519b068b2e4
AlbertoSierraLopez <alberto.sierra360@outlook.com>
2021-10-13 22:03:41 +0200

    Sacar información de los ingredientes y empezar a comprobar requisitos

--- src/ingredient_manager.py
+++ src/ingredient_manager.py
@@ -1,7 +1,9 @@
 import requests
+import nltk
 
 import numpy as np
 import pandas as pd
+
 
 
 class IngredientManager:
@@ -20,46 +22,90 @@ class IngredientManager:
         unwanted_ingredients = []
 
         for ingredient in self.ingredients:
-            info = self.get_nutrients(ingredient)
-            # switch(info): case1: case2: case3:...
+            if not self.passes_requirements(ingredient):
+                unwanted_ingredients.append(ingredient)
 
         return unwanted_ingredients
 
-    # Array de booleanos que señala donde hay elementos del dataframe iguales al ingrediente buscado
-    def get_similarity(self, ingredient, dataframe):
-        similarities = []
-        # Saca el array de similitudes entre el ingrediente y lo que devuelve la query
-        # La idea es que si el ingrediente es 'apple'
-        # podamos separar de la query donde ponga 'APPLE' y donde ponga otras cosas como 'APPLE, JUICE'
-        for word in dataframe['description']:
-            if word.lower() in self.wvmodel.wv.key_to_index:
-                similarities.append(self.wvmodel.wv.similarity(ingredient, word.lower()))
-            else:
-                similarities.append(0)
-        # Devuelve True en las posiciones de elementos que son el ingrediente
-        return np.array(similarities) > 0.9
+    def passes_requirements(self, food):
+        nutrients = self.get_nutrients(food)
+        category = self.get_food_category(food)
+        ingredients = self.get_ingredients(food)
 
-    # Devuelve el dataframe con la información nutricional de una ingrediente dado, o None
-    def get_nutrients(self, ingredient):
+        if 'Dieta vegetariana' in self.requirements:
+            return False
+
+        if 'Dieta vegana' in self.requirements:
+            return False
+
+        if 'Dieta hipocalórica' in self.requirements:
+            return False
+
+        if 'Dieta proteica' in self.requirements:
+            return False
+
+        if 'Dieta baja en carbohidratos' in self.requirements and nutrients is not None:
+            carbs = nutrients.loc[nutrients['nutrientName'] == 'Carbohydrate, by difference']
+            if carbs['value'] > 150.0:
+                return False
+
+        if 'Dieta baja en sodio' in self.requirements and nutrients is not None:
+            sodium = nutrients.loc[nutrients['nutrientName'] == 'Sodium, Na']
+            if sodium['value'] > 100.0:
+                return False
+
+        if 'Intolerancia a la lactosa' in self.requirements:
+            return False
+
+        if 'Alergia los frutos secos' in self.requirements:
+            return False
+
+        if 'Alergia al marisco' in self.requirements:
+            return False
+
+        if 'Intolerancia al gluten' in self.requirements:
+            return False
+
+        if 'Celiaquía' in self.requirements:
+            return False
+
+        return True
+
+    @staticmethod
+    # Devuelve la primera fila de la query como dataframe
+    def get_info(ingredient):
         response = requests.get("https://api.nal.usda.gov/fdc/v1/foods/search?query=" + ingredient +
                                 "&pageSize=10&api_key=dGv22hi1mexUfPPHzeKpENdiVUag9gnFMaEbbKio")
         ingredient_df = pd.json_normalize(response.json()['foods'])
-        similarity = self.get_similarity(ingredient, ingredient_df)
-        # Si el ingrediente no está en base de datos, None
         if len(ingredient_df) > 0:
-            similar_ingredients = ingredient_df.iloc[similarity]
-            # Si en base de datos no salen cosas muy parecidas al ingrediente buscado, None
-            if len(similar_ingredients) > 0:
-                # En caso de que haya varios resultados parecidos a lo que se busca
-                # se devuelve la información nutricional más detallada de entre las de todos ellos (la más larga)
-                nutrients_df = pd.json_normalize(similar_ingredients['foodNutrients'].iloc[
-                                                     np.argmax(similar_ingredients['foodNutrients'].str.len())])
-                return nutrients_df     # La info nutricional es una dataframe
+            return ingredient_df.iloc[0]
 
-    def get_nutrition_dict(self):
-        nutrition_dict = dict()
+    # Devuelve la columna 'Food Nutrients' de la primera fila de la query, en forma de dataframe
+    def get_nutrients(self, ingredient):
+        info = self.get_info(ingredient)
 
-        for ingredient in self.ingredients:
-            nutrition_dict[ingredient] = self.get_nutrients(ingredient)
+        if info is None:
+            return None
 
-        return nutrition_dict
+        nutrients = info['foodNutrients']
+        return pd.json_normalize(nutrients)
+
+    # Devuelve la columna 'Food Category' de la primera fila de la query, tokenizada
+    def get_food_category(self, ingredient):
+        info = self.get_info(ingredient)
+
+        if info is None:
+            return None
+
+        food_cat = info['foodCategory']
+        return nltk.word_tokenize(food_cat)
+
+    # Devuelve la columna 'Ingredients' de la primera fila de la query, tokenizada
+    def get_ingredients(self, ingredient):
+        info = self.get_info(ingredient)
+
+        if info is None:
+            return None
+
+        ingredients = info['ingredients']
+        return nltk.word_tokenize(ingredients)
